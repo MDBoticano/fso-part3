855bd8e077a0f35ad9452644e7c352118bc3de3a
MDBoticano <MDBoticano@users.noreply.github.com>
2019-07-19 16:23:59 -0700

    Exercise 3.6: Phonebook backend step6

--- phonebook/index.js
+++ phonebook/index.js
@@ -51,20 +51,91 @@ let persons = [
   }
 ]
 
-app.get('/api/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+/* Helper Functions */
+const nameExists = (name) => {
+  let existingNames = persons.map(person => person.name.toLowerCase());
+  return existingNames.includes(name.toLowerCase());
+}
+
+const numberExists = (number) => {
+  let existingNumbers = persons.map(person => person.number);
+  return existingNumbers.includes(number);
+}
+
+const generateID = () => {
+  let maxID = -1;
+  if (persons.length > 0) {
+    maxID = Math.max(...persons.map(p => p.id))
+  } else {
+    maxID = 0
+  }
+  const randomNum = Math.floor(Math.random() * (100 - 1) + 1);
+
+  return maxID + randomNum
+}
+
+/* Routes */
+app.get('/api/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
-app.get('/info', (req, res) => {
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.get('/info', (request, response) => {
   let dateOfRequest = new Date();
   let numEntries = persons.length;
   let infoString = 'Phonebook has info for ' + numEntries + ' people'
   
-  res.send('<p>' + infoString + '</p>' + '<p>' + dateOfRequest + '</p>')
+  response.send('<p>' + infoString + '</p>' + '<p>' + dateOfRequest + '</p>')
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if(!body.name || !body.number) {
+    return response.status(400).json({
+      error: "missing content"
+    })
+  } else if (nameExists(body.name)) {
+    return response.status(400).json({
+      error: "name must be unique"
+    })
+  } else if (numberExists(body.number)) {
+    return response.status(400).json({
+      error: "number must be unique"
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+    id: generateID(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 
