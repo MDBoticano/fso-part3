8ea4af501168ed6f53fa5cdbcc03b31e91376907
MDBoticano <MDBoticano@users.noreply.github.com>
2019-07-19 18:01:46 -0700

    Exercise 3.8*: Phonebook backend step8 (morgan middleware for POST)

--- phonebook/index.js
+++ phonebook/index.js
@@ -5,7 +5,10 @@ const morgan = require('morgan')
 
 app.use(bodyParser.json())
 
-app.use(morgan('tiny'))
+morgan.token('body', (request) =>  {
+  return JSON.stringify(request.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 let persons = [
   {
