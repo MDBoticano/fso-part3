325a5da71d00a8092ee0579e38a862777595aee8
MDBoticano <MDBoticano@users.noreply.github.com>
2019-07-23 15:48:32 -0700

    Exercise 3.11 phonebook full stack (Heroku build)

---
+++ phonebook/build/precache-manifest.af0c663e0160c9c6d93973fd9c23ab8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61a668e18bf377718cc2e2c63558a51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98d256825dc656f8310b",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5639b38e2899665f4bb3",
+    "url": "/static/js/2.c834f1ab.chunk.js"
+  },
+  {
+    "revision": "98d256825dc656f8310b",
+    "url": "/static/js/main.6b0c3d91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- phonebook/index.js
+++ phonebook/index.js
@@ -1,9 +1,9 @@
-/* Imports */
+/* Imports & Middleware */
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
-const cors = require('cors')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -13,18 +13,17 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
-/* Middleware */
-app.use(bodyParser.json())
-
 morgan.token('body', (request) =>  {
   return JSON.stringify(request.body)
 })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
+app.use(express.static('build'))
 app.use(cors())
-
+app.use(bodyParser.json())
 app.use(requestLogger)
 
+/* Data */
 let persons = [
   {
     name: "Arto Hellas",
